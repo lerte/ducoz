d952ac3648d8413dd71f898b73196dc0ac96ea89
lerte smith <lerte@qq.com>
2021-06-09 19:28:16 +0800

    use bootstrap 5

--- themes/ducoz/assets/js/main.js
+++ themes/ducoz/assets/js/main.js
@@ -1,6 +1,27 @@
 const app = new Vue({
   el: "#app",
   data: {
-    isActive: false,
+    isDarkMode: false
   },
-});
+  watch: {
+    isDarkMode: function(dark){
+      if(dark){
+        document.body.setAttribute('data-theme', 'dark');
+        localStorage.setItem('darkMode', 'dark');
+      }else{
+        document.body.removeAttribute('data-theme');
+        localStorage.removeItem('darkMode');
+      }  
+    }
+  },
+  methods: {
+    initTheme() {
+      const darkThemeSelected = localStorage.getItem('darkMode') !== null && localStorage.getItem('darkMode') === 'dark';
+      this.isDarkMode = darkThemeSelected;
+      darkThemeSelected ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
+    }   
+  },
+  created(){
+    this.initTheme();
+  }
+})
